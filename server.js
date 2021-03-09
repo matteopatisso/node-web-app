1366cc14a2564c8bb6f6d355a9b03cf77296dac4
Matteo Patisso <matteopatisso@matteos-macbook-pro.home>
2021-03-09 15:39:06 +0100

    Return fetched json

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const fetch = require('node-fetch');
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!!')
+	fetch('https://jsonplaceholder.typicode.com/todos/1')
+	  .then(response => response.json())
+	  .then(json => res.json(json))
 })
 
 app.listen(port, () => {
